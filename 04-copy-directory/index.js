5e662613294cdbb336d2d9da8efae68c50df7784
Katsiaryna <katsiaryna.n@gmail.com>
2023-05-05 15:49:25 +0300

    feat: o4 done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const fs =  require('fs/promises')
+const path = require('path')
 
+const source = path.join(__dirname, 'files');
+const output = path.join(__dirname, 'files-copy');
+
+async function copyDir(from, to) {
+  await fs.rm(to, {recursive:true, force:true})
+  await fs.mkdir(to)
+  const files = await fs.readdir(from) 
+  for(let file of files) {
+    await fs.copyFile(path.join(from, file), path.join(to, file))
+    console.log(`${file} is copied`);
+  }
+}
+copyDir(source, output)
