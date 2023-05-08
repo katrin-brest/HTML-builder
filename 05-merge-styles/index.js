07ca96cceabab1c4d669430b40bcb635570a4191
Katsiaryna <katsiaryna.n@gmail.com>
2023-05-08 11:27:03 +0300

    refactor: 05

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,3 +1,4 @@
+const { error } = require('console')
 const fs =  require('fs/promises')
 const path = require('path')
 
@@ -6,14 +7,10 @@ const styles = path.join(__dirname, 'styles')
 async function mergeStyles (folder) {
   const projectDist = path.join(__dirname, 'project-dist','bundle.css')
   await fs.rm(projectDist, {force:true})
-  const bundle = await fs.writeFile(projectDist, '', 'utf-8')
   const files = await fs.readdir(folder, {withFileTypes:true})
-  for(let file of files) {
-    if(file.isFile() && path.extname(file.name) === '.css') {
-      const style = await fs.readFile(path.join(folder, file.name) , 'utf-8')
-      await fs.appendFile(projectDist, style, 'utf-8')
-     }
-  }
+  const promises = files.filter(file => file.isFile() && path.extname(file.name) === '.css').map( file => fs.readFile(path.join(folder, file.name) , 'utf-8'))
+  let result = await Promise.all(promises)
+  await fs.writeFile(projectDist, result.join('\n'), 'utf-8')
 }
 
-mergeStyles(styles)
+mergeStyles(styles).catch(error => console.log(error))
